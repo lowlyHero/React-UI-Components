3db24821b446d5fa0a6d0562765d1c84ec5c1164
Eric Nunn <lowlyherotech@gmail.com>
2019-05-06 17:01:34 -0400

    Working on styles and content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,10 +3,9 @@ import "./Card.css";
 
 const CardBanner = () => {
   return (
-      <a className="banner-wrapper" href="https://www/reactjs.org">
+      <a className="banner-wrapper" href="https://reactjs.org" target="_blank">
         <img className="banner-img" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"  alt="React Logo" />
       </a>
-
   );
 };
 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
 
 const HeaderTitle = () => {
     return (
         <div className="title-wrapper">
-        <h1>Lambda School</h1>
+         <HeaderContent />
         </div>
     );
 };
