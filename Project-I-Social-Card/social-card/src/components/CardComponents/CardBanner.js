8ffee1c44a5fd9e97022e26c2df7f18ea8e3f71b
Eric Nunn <lowlyherotech@gmail.com>
2019-05-06 17:54:05 -0400

    Sizing elements and aligning content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,7 @@ import "./Card.css";
 
 const CardBanner = () => {
   return (
-      <a className="banner-wrapper" href="https://reactjs.org" target="_blank">
+      <a className="banner-wrapper" href="https://reactjs.org">
         <img className="banner-img" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"  alt="React Logo" />
       </a>
   );
