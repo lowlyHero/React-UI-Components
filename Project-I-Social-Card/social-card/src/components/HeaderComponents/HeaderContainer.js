429c55f9397ebd2bfe2f80c2fd3f6950676e5e43
Eric Nunn <lowlyherotech@gmail.com>
2019-05-06 14:08:18 -0400

    initial commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return (
+        <div className="">
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+
+export default HeaderContainer;
